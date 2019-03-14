0c91009f9160160e4417d90976101b64e4148d2c
tanaka <tanaka@exampe.com>
2019-03-15 07:35:06 +0900

    first commit

---
+++ lib/pdf_kitchen.rb
@@ -0,0 +1,11 @@
+require "pdf_kitchen/version"
+
+module PdfKitchen
+  class Error < StandardError; end
+  # Your code goes here...
+  class Api
+    def initialize(options = {})
+        
+    end
+  end
+end
---
+++ lib/pdf_kitchen/version.rb
@@ -0,0 +1,3 @@
+module PdfKitchen
+  VERSION = "0.1.0"
+end
