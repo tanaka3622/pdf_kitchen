05273d24a15de24a078f4f499060d142ad026e96
tanaka <tanaka@exampe.com>
2019-03-16 16:23:33 +0900

    filename requestBody 追加

--- lib/pdf_kitchen.rb
+++ lib/pdf_kitchen.rb
@@ -1,22 +1,26 @@
 require "pdf_kitchen/version"
 require 'net/http'
 require 'uri'
+require 'json'
 
 module PdfKitchen
   class Error < StandardError; end
   # Your code goes here...
   class Api
-    def self.get(token, secret_token)
+    def self.post(filename: "sample.pdf", access_token:, access_secret_token:, data: {})
         uri = URI.parse('https://dev.pdf.kitchen/app/kitchen/cooking')
         https = Net::HTTP.new(uri.host, uri.port)
         https.use_ssl = true
 
         req = Net::HTTP::Post.new(uri)
         req["Content-Type"] = "application/json"
-        req.basic_auth token, secret_token
-
+        req.basic_auth access_token, access_secret_token
+        req.body = data.to_json
         res = https.request(req)
-        puts res.body
+
+        File.open(filename, "w") do |f|
+          f.puts(res.body)
+        end
     end
   end
 end
